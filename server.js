e370ff11ed95b9f8b38c61730138720c51389602
GinaHobbs <ginamariehobbs@gmail.com>
2021-07-27 18:47:51 -0500

    completed assignment

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ mongoose.connect('mongodb://localhost:27017/db-name', mongooseOptions)
 const app = express();
 app.use(cors());
 
-const PORT = process.env.PORT || 3002;
+const PORT = process.env.PORT || 3001;
 
 const client = jwksClient({
   jwksUri: 'https://dev-pj8m-sfw.us.auth0.com/.well-known/jwks.json'
